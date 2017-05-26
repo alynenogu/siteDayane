07c17748d2f75b5d68426e86d4e5f9f70bb21457
Alyne <alynenogueiragois@gmail.com>
2017-05-26 16:48:40 -0300

    Começando o css

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-
+<!DOCTYPE html>
 <html>
-<head>
-
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>Dayane Maquiagem</title>
+		<link rel="icon" href="css/imagens/logotipo_dayanemaquiagem.jpg">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
 <body>
-<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo" width="200px" height="200px">
-<h1> Sobre mim ...</h1>
-<p> Moro em campo Grande....</p>
+	<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo" width="200px" height="200px">
+		<h1 > Sobre mim ...</h1>
+	<p> Moro em campo Grande....</p>
 
-<h2> Como trabalho </h2>
+		<h2 > Como trabalho </h2>
 
-<h3> Experiências </h3>
+		<h3> Experiências </h3>
 
 
 <ul>
