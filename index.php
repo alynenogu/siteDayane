2e883d98a734ca12fa4075ba7699c412beb8c2da
root <root@localhost.localdomain>
2017-05-26 15:59:05 -0300

    meu primeiro commit mesmo

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+
+</head>
+<body>
+<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo" width="200px" height="200px">
+<h1> Sobre mim ...</h1>
+<p> Moro em campo Grande....</p>
+
+<h2> Como trabalho </h2>
+
+<h3> Experiências </h3>
+
+
+<ul>
+	<li>a</li>
+</ul>
+
+</body>
+
+</html>
+
