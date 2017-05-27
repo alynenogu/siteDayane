ff218e15074addf0c1b511cbbc18e2ac44d845ee
Alyne <alynenogueiragois@gmail.com>
2017-05-27 11:03:33 -0300

    Alterações no css e tbm coloquei algumas imagens

--- index.php
+++ index.php
@@ -5,20 +5,44 @@
 		<title>Dayane Maquiagem</title>
 		<link rel="icon" href="css/imagens/logotipo_dayanemaquiagem.jpg">
 		<link rel="stylesheet" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
 	</head>
 <body>
+	<main>
 	<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo" width="200px" height="200px">
 		<h1 > Sobre mim ...</h1>
 	<p> Moro em campo Grande....</p>
 
 		<h2 > Como trabalho </h2>
+		<blockquote>
+			<p>Make de qualidade feita pela Dayane Rocha Make up! Recomendo!</p>
+			<cite>Camilla de Paula, Rio de janeiro</cite>
+		</blockquote>
 
-		<h3> Experiências </h3>
+		<h2> Experiências </h2>
+		<blockquote>
+			<p>Uma maquiadora super dedicada</p>
+			<cite>Jojo, Rio de Janeiro</cite>
+		</blockquote>
+		<p> trabalhei para as minhas <a href="#">cunhadas</a>
+		</main>
+		<img src="css/imagens/Dayane.jpg" alt="Dayane Rocha">
+<nav>
+	<ul>
+		<h1> Dayane Rocha Make up </h1> 
+<li><a href="#">Home</a></li>
+<li><a href="#">Fotos dos meus trabalhos</a></li>
 
 
-<ul>
-	<li>a</li>
-</ul>
+
+
+	</ul>
+</nav>
+
+<footer>
+	&copy;Alyne Nogueira Gois - 2017
+
+</footer>
 
 </body>
 
