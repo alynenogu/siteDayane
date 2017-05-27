ce68ad9c51527b8e403cf36ca44f2745f58d768d
Alyne <alynenogueiragois@gmail.com>
2017-05-27 14:57:49 -0300

    Icones das Redes sociais e ajustes css

--- index.php
+++ index.php
@@ -4,40 +4,72 @@
 		<meta charset="UTF-8">
 		<title>Dayane Maquiagem</title>
 		<link rel="icon" href="css/imagens/logotipo_dayanemaquiagem.jpg">
+		<link rel="stylesheet" href="css/reset.css">
 		<link rel="stylesheet" href="css/style.css">
+
 		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
 	</head>
 <body>
 	<main>
 	<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo" width="200px" height="200px">
 		<h1 > Sobre mim ...</h1>
-	<p> Moro em campo Grande....</p>
+	<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.
+	 Proin bibendum viverra felis, sed viverra velit aliquam id. 
+	 Cras eget nunc lacus. In viverra purus eget pellentesque consectetur.
+	  Pellentesque lacinia nibh sit amet neque ultricies pellentesque. 
+	  Integer est neque, laoreet vitae egestas at, dignissim eu dolor. 
+	  Praesent semper ex ut sapien scelerisque finibus. 
+</p>
 
 		<h2 > Como trabalho </h2>
+		<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.
+	 Proin bibendum viverra felis, sed viverra velit aliquam id. 
+	 Cras eget nunc lacus. In viverra purus eget pellentesque consectetur.
+	  Pellentesque lacinia nibh sit amet neque ultricies pellentesque. 
+	  Integer est neque, laoreet vitae egestas at, dignissim eu dolor. 
+	  Praesent semper ex ut sapien scelerisque finibus. 
+</p>
 		<blockquote>
 			<p>Make de qualidade feita pela Dayane Rocha Make up! Recomendo!</p>
 			<cite>Camilla de Paula, Rio de janeiro</cite>
 		</blockquote>
 
 		<h2> Experiências </h2>
+		<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.
+	 Proin bibendum viverra felis, sed viverra velit aliquam id. 
+	 Cras eget nunc lacus. In viverra purus eget pellentesque consectetur.
+	  Pellentesque lacinia nibh sit amet neque ultricies pellentesque. 
+	  Integer est neque, laoreet vitae egestas at, dignissim eu dolor. 
+	  Praesent semper ex ut sapien scelerisque finibus. 
+</p>
 		<blockquote>
 			<p>Uma maquiadora super dedicada</p>
 			<cite>Jojo, Rio de Janeiro</cite>
 		</blockquote>
-		<p> trabalhei para as minhas <a href="#">cunhadas</a>
+	
 		</main>
-		<img src="css/imagens/Dayane.jpg" alt="Dayane Rocha">
+
+<aside>
+	<img src="css/imagens/Dayane.jpg" alt="Dayane Rocha"  width="200px" height="200px">
+<h1> Dayane Rocha Make up </h1> 
 <nav>
+
 	<ul>
-		<h1> Dayane Rocha Make up </h1> 
+		
 <li><a href="#">Home</a></li>
 <li><a href="#">Fotos dos meus trabalhos</a></li>
-
+<li><a href="bio.html">Sobre mim</a></li>
+<li><a href="contato.html">Contato</a></li>
 
 
 
 	</ul>
 </nav>
+<ul>
+<li><a href="https://www.facebook.com/dayane.rocha.makeup/"><img src="css/imagens/images.png" alt="facebook" width="40px" height="27px"></a></li>
+<li><a href="https://www.instagram.com/day.rocha.makeup"><img src="css/imagens/instagram_PNG10.png" alt="instagram" width="40px" height="27px"></a></li>
+</ul>
+</aside>
 
 <footer>
 	&copy;Alyne Nogueira Gois - 2017
