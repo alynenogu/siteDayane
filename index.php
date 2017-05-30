14998af37962bd98a5f4c9065eb6ba75265e02d3
Alyne <alynenogueiragois@gmail.com>
2017-05-30 17:02:37 -0300

    Terminando os ultimos detalhes

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 	</head>
 <body>
 	<main>
-	<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo" width="200px" height="200px">
-		<h1 > Sobre mim ...</h1>
+<img src = "css/imagens/logotipo_dayanemaquiagem.jpg" alt="Logotipo"  class="logotipo">
+		<h1 class="titulo-principal"> Sobre mim ...</h1>
 	<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.
 	 Proin bibendum viverra felis, sed viverra velit aliquam id. 
 	 Cras eget nunc lacus. In viverra purus eget pellentesque consectetur.
@@ -21,7 +21,7 @@
 	  Praesent semper ex ut sapien scelerisque finibus. 
 </p>
 
-		<h2 > Como trabalho </h2>
+		<h2 class="subtitulo" > Como trabalho </h2>
 		<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.
 	 Proin bibendum viverra felis, sed viverra velit aliquam id. 
 	 Cras eget nunc lacus. In viverra purus eget pellentesque consectetur.
@@ -29,28 +29,39 @@
 	  Integer est neque, laoreet vitae egestas at, dignissim eu dolor. 
 	  Praesent semper ex ut sapien scelerisque finibus. 
 </p>
-		<blockquote>
+<blockquote class="citacao">
+			<p>Uma maquiadora super dedicada</p>
+			<cite>Jojo, Rio de Janeiro</cite>
+		</blockquote>
+
+		<blockquote class="citacao">
 			<p>Make de qualidade feita pela Dayane Rocha Make up! Recomendo!</p>
 			<cite>Camilla de Paula, Rio de janeiro</cite>
 		</blockquote>
 
-		<h2> Experiências </h2>
-		<p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.
+		<blockquote class="citacao">
+			<p>Uma maquiadora que eu admiro e indico!</p>
+			<cite>Fulano de Tal, Rio de Janeiro</cite>
+		</blockquote>
+
+		<h2 class="subtitulo"> Experiências </h2>
+		<p class="texto_final">  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a sem non ex tristique rutrum.Proin bibendum viverra felis, sed viverra velit aliquam id. 
 	 Proin bibendum viverra felis, sed viverra velit aliquam id. 
 	 Cras eget nunc lacus. In viverra purus eget pellentesque consectetur.
 	  Pellentesque lacinia nibh sit amet neque ultricies pellentesque. 
 	  Integer est neque, laoreet vitae egestas at, dignissim eu dolor. 
 	  Praesent semper ex ut sapien scelerisque finibus. 
 </p>
-		<blockquote>
-			<p>Uma maquiadora super dedicada</p>
-			<cite>Jojo, Rio de Janeiro</cite>
+	<blockquote class="citacao">
+			<p>Uma maquiadora estudiosa!</p>
+			<cite>Fulano de Tal, Rio de Janeiro</cite>
 		</blockquote>
+		
 	
 		</main>
-
-<aside>
-	<img src="css/imagens/Dayane.jpg" alt="Dayane Rocha"  width="200px" height="200px">
+<img src="css/imagens/Dayane.jpg" alt="Dayane Rocha"  class="foto-dayane">
+<aside class="navegacao-site">
+	
 <h1> Dayane Rocha Make up </h1> 
 <nav>
 
@@ -65,13 +76,18 @@
 
 	</ul>
 </nav>
-<ul>
-<li><a href="https://www.facebook.com/dayane.rocha.makeup/"><img src="css/imagens/images.png" alt="facebook" width="40px" height="27px"></a></li>
+<ul class="redes-sociais">
+<!--<li><a href="https://www.facebook.com/dayane.rocha.makeup/"><img src="css/imagens/images.png" alt="facebook" width="40px" height="27px"></a></li>
 <li><a href="https://www.instagram.com/day.rocha.makeup"><img src="css/imagens/instagram_PNG10.png" alt="instagram" width="40px" height="27px"></a></li>
+!-->
+<li><a class="facebook" href="https://www.facebook.com/dayane.rocha.makeup/"> Facebook</a></li>
+<li><a class="instagram" href="https://www.instagram.com/day.rocha.makeup">Instagram</a></li>
+
+
 </ul>
 </aside>
 
-<footer>
+<footer class="rodape">
 	&copy;Alyne Nogueira Gois - 2017
 
 </footer>
